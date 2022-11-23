6a97e4a94f5dee07905ffbeb9f7885b54fff97ac
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-11-24 02:16:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Number.isInteger(Math.random() * 10) +1;
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3%num4;
+
+let max = Math.max(5,7,14,12,16,3,9,20)
+
+max;
+
